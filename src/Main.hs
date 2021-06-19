6448e9ecf2ef5365123fe5e9a7f446e47bb22174
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-06-19 01:48:21 -0500

    more stuff 😩

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,20 @@
 module Main where
 
+import Calamity (EventType (MessageCreateEvt), Message (Message), Token (BotToken), defaultIntents, react, runBotIO, tell)
+import Calamity.Cache.InMemory (runCacheInMemory)
+import Calamity.Commands (addCommands)
+import Calamity.Metrics.Noop (runMetricsNoop)
 import Control.Concurrent (threadDelay)
-import System.Directory ( removeFile )
+import Control.Lens ()
+import Control.Monad (void)
+import Data.Default ()
+import Data.Generics.Labels ()
+import Data.Maybe ()
+import Data.Text.Lazy (Text, pack)
+import qualified Di
+import DiPolysemy (info, runDiToIO)
+import Polysemy (embedToFinal, runFinal)
+import System.Directory (removeFile)
 import System.IO (hGetContents)
 import System.Process
   ( CreateProcess (std_out),
@@ -12,23 +25,6 @@ import System.Process
   )
 import System.Random (randomIO)
 import Text.Regex (Regex, matchRegex, mkRegex, subRegex)
--- some calamity shitty stuff
-
-
-import           Calamity
-import           Calamity.Cache.InMemory
-import           Calamity.Commands
-import           Calamity.Metrics.Noop
-import           Control.Lens
-import           Control.Monad
-import           Data.Default
-import           Data.Generics.Labels    ()
-import           Data.Maybe
-import           Data.Text.Lazy          (Text)
-import qualified Data.Text.Lazy          as T
-import qualified Di
-import           DiPolysemy
-import qualified Polysemy                as P
 
 {-
 -- this is for replace this kinda stuff
@@ -46,7 +42,6 @@ replaceSomeShittyStuff x =
     Nothing -> x
     Just _ -> replaceSomeShittyStuff (subRegex regexReadAndWrite x "")
 
-
 {-
 $compile ```hs
 main=print "hello world"
@@ -54,6 +49,7 @@ main=print "hello world"
 -}
 getCode :: String -> String
 getCode x = replaceSomeShittyStuff $drop 14 $take (length x - 3) x
+
 executeCode :: String -> IO String
 executeCode x = do
   id <- randomIO :: IO Int
@@ -63,19 +59,19 @@ executeCode x = do
   (_, Just outHandle, _, ph) <-
     createProcess
       (proc "sh" ["-c", "echo main | ghci " ++ "./src/files/" ++ show id ++ ".hs -no-global-package-db -no-user-package-db"])
-        { std_out = CreatePipe-- we use a pipe because i dont want to stop the program if someone insert something like this
-          {--
-          recusiveShittyStuff=do
-            print "penis"
-            recusiveShittyStuff
+        { std_out = CreatePipe -- we use a pipe because i dont want to stop the program if someone insert something like this
+        {--
+        recusiveShittyStuff=do
+          print "penis"
+          recusiveShittyStuff
 
-          --}
+        --}
         }
   -- then we get the output
   out <- hGetContents outHandle
   -- we wait a second
   threadDelay 1000000
-  -- i terminate the process if someone insert me something like idk 
+  -- i terminate the process if someone insert me something like idk
   {--
         recusiveShittyStuff=do
           print "penis"
@@ -86,28 +82,32 @@ executeCode x = do
   removeFile ("./src/files/" ++ show id ++ ".hs")
   -- and we get the output
   return $drop 180 $ take (length out -21) out
-  -- maybe for u its a little bit weird that i take a slice of the string but it have a reason, 
-  -- because if i dont take that slice it returns me something like this
-  {--
-  GHCi, version 8.10.4: https://www.haskell.org/ghc/  :? for help
-  [1 of 1] Compiling Main             ( src/files/-4887131526902941531.hs, interpreted )
-  Ok, one module loaded.
-  *Main> 12
-  *Main> Leaving GHCi.
-  --} 
 
+-- maybe for u its a little bit weird that i take a slice of the string but it have a reason,
+-- because if i dont take that slice it returns me something like this
+{--
+GHCi, version 8.10.4: https://www.haskell.org/ghc/  :? for help
+[1 of 1] Compiling Main             ( src/files/-4887131526902941531.hs, interpreted )
+Ok, one module loaded.
+*Main> 12
+*Main> Leaving GHCi.
+--}
 
+runBot :: Text -> IO ()
+runBot token = Di.new $ \di ->
+  void
+    . runFinal
+    . embedToFinal @IO
+    . runDiToIO di
+    . runCacheInMemory
+    . runMetricsNoop
+    . runBotIO (BotToken token) defaultIntents
+    $ do
+      info @Text "Connected successfully."
+      addCommands $ do
+        command @'[KleenePlusConcat Text] "yourcommand" $ \ctx rest -> do
+
+main :: IO ()
 main = do
-  token<-readFile "token.txt"
-
-  Di.new $ \di ->
-    void
-  . P.runFinal
-  . P.embedToFinal @IO
-  . runDiToIO di
-  . runCacheInMemory
-  . runMetricsNoop
-  . useConstantPrefix "!"
-  . runBotIO (BotToken t) defaultIntents
-  $ do
-    info @Text "Connected successfully."
+  token <- readFile "token.txt"
+  runBot $ pack token
