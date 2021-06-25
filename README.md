dd8654a282d25ee2ef29c7eded83ee654cdbb091
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-06-25 17:16:34 -0500

    some new commands and cool stuff

--- README.md
+++ README.md
@@ -6,15 +6,21 @@ I couldn't have done this project without [discord-haskell](https://hackage.hask
 ## How it looks
 | simple function| error| infinite loop|
 |--|--|--|
-|![img](https://media.discordapp.net/attachments/820472030474272769/857759230135762964/Screen_Shot_2021-06-24_at_18.08.10.png?width=1237&height=941)|![img](https://media.discordapp.net/attachments/820472030474272769/857762776840011786/Screen_Shot_2021-06-24_at_18.22.50.png?width=1232&height=943)|![img](https://media.discordapp.net/attachments/820472030474272769/857521599782715392/Screen_Shot_2021-06-24_at_2.24.29.png)
+|![img](https://media.discordapp.net/attachments/820472030474272769/857759230135762964/Screen_Shot_2021-06-24_at_18.08.10.png?width=1237&height=941)|![img](https://media.discordapp.net/attachments/820472030474272769/857762776840011786/Screen_Shot_2021-06-24_at_18.22.50.png?width=1232&height=943)|![img](https://media.discordapp.net/attachments/820472030474272769/858101201711005746/Screen_Shot_2021-06-25_at_16.47.33.png)
 
-
+|list of commands| function|
+|--|--|
+|$help |this command is for receive help lmao|
+|$github |if you want to see the source code|
+|$server |if you want to join us|
+|$compile |this compile the input ant then return the output|
 ## How to deploy it
 
 first u need the [haskell dev kit](https://www.haskell.org/downloads/#linux-mac-freebsd) and  install [stack](https://docs.haskellstack.org/en/stable/README/) then put the token  in the file `token.txt` .After that you need to run the command 
 ```bash
 stack run
 ```
+
 
 ## Me after made this project
 ![image](https://media1.tenor.com/images/9385eb15bbb7a5d76357a1a59544b072/tenor.gif?itemid=12496470)
--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import Control.Concurrent (threadDelay)
 import Control.Monad (unless, when)
 import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.List.Split (splitOn)
 import Data.Text (Text, pack, toLower, unpack)
 import qualified Data.Text.IO as TIO
 import Discord (DiscordHandler, RunDiscordOpts (discordOnEvent, discordToken), def, restCall, runDiscord)
@@ -27,9 +28,10 @@ replaceSomeShittyStuff x =
   case matchRegex regexReadAndWrite x of
     Nothing -> x
     Just _ -> replaceSomeShittyStuff (subRegex regexReadAndWrite x "")
-    
+
 find :: Eq a => [a] -> [a] -> Bool
-find x  y= any (\ a -> take (length y) ( drop a x) == y) [0 .. (length x)]
+find x y = any (\a -> take (length y) (drop a x) == y) [0 .. (length x)]
+
 {-
 $compile ```hs
 main=print "hello world"
@@ -70,21 +72,21 @@ executeCode x = do
           (messageChannel x)
           "output"
           $def
-            { 
-              createEmbedAuthorName = "server",
+            { createEmbedAuthorName = "server",
               createEmbedAuthorUrl = "https://discord.gg/e52RFh7Cg2",
-              createEmbedTitle = if not$find outIO "Failed" then "Big brain Moment" else "Error 😩",
+              createEmbedTitle = if not $find outIO "Failed" then "Big brain Moment" else "Error 😩",
               createEmbedDescription =
                 pack $
                   "```"
-                    ++ ( if not$find outIO "Failed"
-                           then take 500 (drop 180 $ take (length outIO -21) outIO)
-                           else "hs\n" ++ take 500 errIO++"\n"
+                    ++ ( if not $find outIO "Failed"
+                           then take 500 (drop 179 $ take (length outIO -21) outIO)
+                           else "hs\n" ++ take 500 errIO ++ "\n"
                        )
                     ++ "```",
-              createEmbedImage = if find outIO "Failed" then
-                 Just $ CreateEmbedImageUrl "https://media1.tenor.com/images/039d5fa4895c07d58b8c88e69847cf16/tenor.gif?itemid=17634321"
-                 else Just $ CreateEmbedImageUrl "https://i0.wp.com/media1.tenor.com/images/a7215e2bf39482df8bb694f132af5c78/tenor.gif?itemid=16327782?resize=91,91"
+              createEmbedImage =
+                if find outIO "Failed"
+                  then Just $ CreateEmbedImageUrl "https://media1.tenor.com/images/039d5fa4895c07d58b8c88e69847cf16/tenor.gif?itemid=17634321"
+                  else Just $ CreateEmbedImageUrl "https://i0.wp.com/media1.tenor.com/images/a7215e2bf39482df8bb694f132af5c78/tenor.gif?itemid=16327782?resize=91,91"
             }
 
   removeFile nameCode
@@ -100,19 +102,35 @@ eventHandler :: Event -> DiscordHandler ()
 eventHandler event =
   case event of
     MessageCreate m -> when (not (fromBot m) && take 1 (unpack $messageText m) == "$") $ do
-      if take 8 (unpack $messageText m) == "$compile"
-        then do
+      case head $splitOn " " $unpack $messageText m of
+        "$compile" -> do
           _ <- restCall (R.CreateReaction (messageChannel m, messageId m) "eyes")
           messReq <- liftIO (executeCode m)
           _ <- restCall messReq
           pure ()
-        else
-          if take 7 (unpack $messageText m) == "$serve"
-            then do
-              _ <- restCall (R.CreateMessage (messageChannel m) "https://discord.gg/e52RFh7Cg2")
-              pure ()
-            else pure ()
-      pure ()
+        "$server" -> do
+          _ <- restCall (R.CreateMessage (messageChannel m) "https://discord.gg/e52RFh7Cg2")
+          pure ()
+        "$help" -> do
+          _ <-
+            restCall
+              ( R.CreateMessageEmbed
+                  (messageChannel m)
+                  ""
+                  $def
+                    { createEmbedAuthorName = "server",
+                      createEmbedAuthorUrl = "https://discord.gg/e52RFh7Cg2",
+                      createEmbedTitle ="list of commands",
+                      createEmbedDescription="```md\n- $help *this command is for receive help lmao*\n- $github *if you want to see the source code*\n- $server *if you want to join us*\n- $compile *this compile the input ant then return the output* ```",
+                      createEmbedImage = Just $ CreateEmbedImageUrl "https://media.discordapp.net/attachments/820472030474272769/858103586511519744/Screen_Shot_2021-06-25_at_16.57.05.png"
+                    }
+              )
+          pure ()
+        "$github"->do
+          _ <- restCall (R.CreateReaction (messageChannel m, messageId m) "hearts")
+          _<-restCall (R.CreateMessage (messageChannel m) "https://github.com/ranon-rat/haskell-discord-compiler")
+          pure()
+        _ -> pure ()
     _ -> pure ()
 
 fromBot :: Message -> Bool
@@ -124,51 +142,3 @@ main = do
 
   token <- readFile "token.txt"
   runBot $ pack token
-
-{-
-executeCode :: Message -> IO (ChannelRequest Message)
-executeCode x = do
-  id <- randomIO :: IO Int
-
-  let nameCode = "./src/files/" ++ show (if id < 0 then id * (-1) else id) ++ ".hs"
-      nameOut = "src/files/" ++ show (if id < 0 then id * (-1) else id) ++ ".txt"
-
-  writeFile nameCode (replaceSomeShittyStuff $getCode $unpack $messageText x)
-  (_, Just outHandler, Just errHandle, ph) <-
-    createProcess
-      (proc "sh" ["-c", "echo main | ghci " ++ nameCode ++ " -no-global-package-db -no-user-package-db > " ++ nameOut ++ " & sleep 3;kill $! "])
-        { std_out = CreatePipe,
-          std_err = CreatePipe
-        }
-
-  threadDelay $ 3 * 10 ^ 6
-  terminateProcess ph
-  outIO <- readFile nameOut
-
-  errIO <- hGetContents errHandle
-  -- src/files/6027314577073774077.hs
-  -- echo main | ghci src/files/6027314577073774077.hs -no-global-package-db -no-user-package-db > src/files/6027314577073774077 & sleep 1;kill $!
-  d <- hGetContents outHandler
-
-  print errIO
-  let message =
-        R.CreateMessageEmbed
-          (messageChannel x)
-          "output"
-          $def
-            { createEmbedTitle = if null errIO then "big brain :)" else "error 😭",
-              createEmbedDescription = pack $ "```" ++ (if null errIO then
-                "\n" ++ take 500 (drop 180 $ take (length outIO -21) outIO)
-                else "hs\n" ++ take 500 errIO) ++ "```",
-              createEmbedImage = if not $null errIO then
-                 Just $ CreateEmbedImageUrl "https://media1.tenor.com/images/039d5fa4895c07d58b8c88e69847cf16/tenor.gif?itemid=17634321"
-                 else Just $ CreateEmbedImageUrl "https://i0.wp.com/media1.tenor.com/images/a7215e2bf39482df8bb694f132af5c78/tenor.gif?itemid=16327782?resize=91,91",
-              createEmbedAuthorName = "server",
-              createEmbedAuthorUrl = "https://discord.gg/e52RFh7Cg2"
-            }
-
-  removeFile nameCode
-  removeFile nameOut
-  return message
-
--}
